957eae33dbab853eab049e2b0f42e039347bed34
Guilherme Perlasca <perlasca47@gmail.com>
2025-03-25 15:23:34 -0300

    primeiro commit

---
+++ IPHONE/scripts.js
@@ -0,0 +1,8 @@
+function trocacor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+function trocaimagem(imagem) {
+    let imgIphone = document.querySelector(".iphone-azul")
+    imgIphone.src = imagem
+}
